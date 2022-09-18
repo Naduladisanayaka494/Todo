9265eac601ed2301f8e6fdf7efda52294bc8b45f
Naduladisanayaka494 <it20247140@my.sliit.lk>
2022-09-18 09:10:02 +0530

    first commit

---
+++ resources/views/libraries/styles.blade.php
@@ -0,0 +1,5 @@
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+<script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+@stack('css')
---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12 text-center">
+            <h1 class="page-title">Home page</h1>
+
+
+
+
+
+        </div>
+
+
+
+    </div>
+
+
+
+
+  </div>
+
+@endsection
+
+@push('css')
+<style>
+    .page-title{
+        padding-top: 15vh;
+        font-size:5rem;
+        color:dimgrey;
+        
+
+    }
+
+</style>
+@endpush
